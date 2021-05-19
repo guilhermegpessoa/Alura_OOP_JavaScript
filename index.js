94fb2d0d006135e61d02cdc21c64bd01a4f59161
Guilherme G. Pessoa <gpessoa@ciandt.com>
2021-05-19 11:56:05 -0300

    Aula 03: Modularizando código

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-class Cliente{
-  nome;
-  cpf;
-}
-
-class ContaCorrente{
-  agencia;
-  _saldo = 0;
-
-  sacar(valor){
-    if(this._saldo >= valor){
-      this._saldo -= valor;
-      return valor;
-    }
-  }
-
-  depositar(valor){
-    if(valor > 0){
-      this._saldo += valor;
-    }
-  }
-}  
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js" 
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -31,8 +11,15 @@ cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(100);
-const valorSacado = contaCorrenteRicardo.sacar(50);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
-console.log(valorSacado);
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
+
+console.log(conta2);
+console.log(contaCorrenteRicardo);
